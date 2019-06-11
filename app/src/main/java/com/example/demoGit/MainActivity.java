0555cb656d9ab03283e03b73541ba0c57cbf82c7
zong <1546227652@qq.com>
2019-06-11 16:00:45 +0800

    李四没有更新就提交

--- app/src/main/java/com/example/demoGit/MainActivity.java
+++ app/src/main/java/com/example/demoGit/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
                     //呵呵44 你哈啊
                     //张三没更新就修改提交了
                     //张三提交被拒绝 然后重新修改提交
+                    //李四没有更新就提交
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
